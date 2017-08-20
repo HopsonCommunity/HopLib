55d5096b6eba84a7d3eedde5a4eabbce5d35e3cf
Hopson97 <mhopson@hotmail.co.uk>
2017-08-20 15:54:25 +0100

    first commit

---
+++ Source/Memory.h
@@ -0,0 +1,6 @@
+#ifndef MEMORY_H_INCLUDED
+#define MEMORY_H_INCLUDED
+
+#include "Memory/UniquePtr.h"
+
+#endif // MEMORY_H_INCLUDED
---
+++ Source/Memory/UniquePtr.h
@@ -0,0 +1,13 @@
+#ifndef UNIQUEPTR_H_INCLUDED
+#define UNIQUEPTR_H_INCLUDED
+
+namespace HopLib
+{
+    template<typename T>
+    class UniquePtr
+    {
+
+    };
+}
+
+#endif // UNIQUEPTR_H_INCLUDED
